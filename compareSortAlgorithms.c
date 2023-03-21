69b55656cfb6bc963454b8aa0e442eb06cb7aaec
sid8285 <122946547+sid8285@users.noreply.github.com>
2023-03-21 10:44:23 -0400

    Update compareSortAlgorithms.c

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -97,8 +97,11 @@ void insertionSort(int* pData, int n)
 void bubbleSort(int* pData, int n)
 {
 	int i, j;
+	//outer loop to determine the number of passes needed to prperly sort the array
     for (i = 0; i < n - 1; i++) {
+	    //inner loop used to go through the array index for comparison purposes
         for (j = 0; j < n - i - 1; j++) {
+		//the comparison process; seeing if the space before is greater than the space after and swapping the value if so
             if (pData[j] > pData[j + 1]) {
                 int temp = pData[j];
                 pData[j] = pData[j + 1];
@@ -115,13 +118,16 @@ void bubbleSort(int* pData, int n)
 void selectionSort(int* pData, int n)
 {
 	int i, j, min_idx;
+	//outer loop to make the correct number of passes for the given array
     for (i = 0; i < n - 1; i++) {
         min_idx = i;
+	    //inner loop and logic to compare values and sort
         for (j = i + 1; j < n; j++) {
             if (pData[j] < pData[min_idx]) {
                 min_idx = j;
             }
         }
+	    //swapping technique to sort the variables correctly
         int temp = pData[min_idx];
         pData[min_idx] = pData[i];
         pData[i] = temp;
